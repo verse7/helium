4bbc6e792a48c724b0023a36fef65d973e85c8e4
Kayon-Marie <k.marie.douglas@hotmail.com>
2019-06-11 15:19:34 -0400

    new create account format, input masking

--- src/main.js
+++ src/main.js
@@ -6,11 +6,13 @@ import "./registerServiceWorker";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faUserSecret, faClipboardList, faUser, faEnvelope, faUnlockAlt, faPhoneAlt, faAddressBook, faMapMarkerAlt } from "@fortawesome/free-solid-svg-icons";
+import { faUserSecret, faClipboardList, faUser, faEnvelope, faUnlockAlt, faPhoneAlt, faAddressBook, faMapMarkerAlt, faSearch } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+const VueInputMask = require('vue-inputmask').default;
 
-library.add(faUserSecret, faClipboardList, faUser, faEnvelope, faUnlockAlt, faPhoneAlt, faAddressBook, faMapMarkerAlt);
+library.add(faUserSecret, faClipboardList, faUser, faEnvelope, faUnlockAlt, faPhoneAlt, faAddressBook, faMapMarkerAlt, faSearch);
 
+Vue.use(VueInputMask);
 Vue.component("font-awesome-icon", FontAwesomeIcon);
 
 Vue.config.productionTip = false;
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="about">
+  <div>
     <div class="format-form">
-      <h5 class="font-weight-bold ml-3 mt-3">Create Account</h5>
-      <div class="alert alert-danger container mt-5" role="alert" v-if='error'>
+      <h4 class="font-weight-bold ml-3">Create Account</h4>
+      <div class="alert alert-danger container mt-2" role="alert" v-if='error'>
         {{ message }}
       </div>
       <div v-else>
-        <div class="alert alert-success container mt-5" role="alert" v-if='success'>
+        <div class="alert alert-success container mt-2" role="alert" v-if='success'>
           {{ notifs }}
         </div>
       </div>
-      <div class="col-md-6 mt-4 bg-white shadow-sm rounded">
+      <div class="col-md-6 mt-4 bg-white shadow-sm rounded border-top">
         <form id="registerForm" method="post" @submit.prevent="register">
-          <div class="form-row pb-2 pt-3">
+          <div class="form-row pb-2 pt-3 pr-2 pl-2">
             <div class="col pb-2 input-group">
               <div class="input-group-prepend">
                 <div class="input-group-text"><font-awesome-icon icon="user"></font-awesome-icon></div>
@@ -23,35 +23,35 @@
               <input type="text" class="form-control" id="lname" placeholder="Lastname">
             </div>
           </div>
-          <div class="form-group input-group pb-2">
+          <div class="form-group input-group pb-2 pr-2 pl-2">
             <div class="input-group-prepend">
               <div class="input-group-text"><font-awesome-icon icon="envelope"></font-awesome-icon></div>
             </div>
-            <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Email Address">
+            <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Email Address" required>
           </div>
-          <div class="form-group input-group pb-2">
+          <div class="form-group input-group pb-2 pr-2 pl-2">
             <div class="input-group-prepend">
               <div class="input-group-text"><font-awesome-icon icon="unlock-alt"></font-awesome-icon></div>
             </div>
-            <input type="password" class="form-control" id="password" placeholder="Password">
+            <input type="password" class="form-control" id="password" placeholder="Password" required>
           </div>
-          <div class="form-group input-group pb-2">
+          <div class="form-group input-group pb-2 pr-2 pl-2">
             <div class="input-group-prepend">
               <div class="input-group-text"><font-awesome-icon icon="phone-alt"></font-awesome-icon></div>
             </div>
-            <input type="tel" class="form-control" id="phone" placeholder="Telephone">
+            <input type="text" class="form-control" id="phone" placeholder="Telephone" v-mask="{mask: ['(999) 999-9999']}">
           </div>
           <small class="text-muted">Providing a valid trn number allows us to verify who you are and helps us to appropriate the sites content</small>
-          <div class="form-group pb-2 pt-3">
-            <input type="text" class="form-control rounded" id="trn" placeholder="TRN (123-456-789)">
+          <div class="form-group pb-2 pt-3 pr-2 pl-2">
+            <input type="text" class="form-control rounded" id="trn" placeholder="TRN" v-mask="{mask: ['999-999-999']}">
           </div>
-          <div class="form-group input-group pb-2">
+          <div class="form-group input-group pb-2 pr-2 pl-2">
             <div class="input-group-prepend">
               <div class="input-group-text"><font-awesome-icon icon="address-book"></font-awesome-icon></div>
             </div>
             <input type="text" class="form-control" id="address" placeholder="Address">
           </div>
-          <div class="form-row pb-4">
+          <div class="form-row pb-4 pr-2 pl-2">
             <div class="form-group input-group col-md-6">
               <div class="input-group-prepend">
                 <div class="input-group-text"><font-awesome-icon icon="map-marker-alt"></font-awesome-icon></div>
@@ -61,18 +61,31 @@
             <div class="form-group col-md-4">
               <select id="parish" class="form-control">
                 <option selected>Parish</option>
-                <option>...</option>
+                <option v-for="p in parishes" v-bind:key="p">{{ p }}</option>
               </select>
             </div>
             <div class="form-group col-md-2">
               <input type="text" class="form-control" id="inputZip" placeholder="Zip">
             </div>
           </div>
-          <div class="form-group pb-4">
+          <div class="form-group pb-4 pr-2 pl-2">
             <input type="submit" value="Submit" class="btn btn-block btn-color rounded-pill font-weight-bold">
           </div>
         </form>
       </div>
     </div>
+    <div class="bg box"></div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      parishes: ["Kingston","St. Andrew","St. Catherine","Trelawny","St. Ann<","PortLand","St. Mary",
+      "St. Thomas","Clarendon","St. Elizabeth","St. James","Manchester","Hanover","Westmoreland"]
+    }
+  }
+}
+</script>
+
